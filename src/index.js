1c526fb8b04f3e701d77b400c3fa5a329647e928
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-03-01 22:39:00 -0500

    project architecture created

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
   </React.StrictMode>
 );
