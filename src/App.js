418dd2dd827a297061e0daa13bfaf565fce14b6f
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-03-11 01:59:52 -0500

    sidebar done

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import {
     ColorPicker,
     ColorMapping,
     Editor,
+    Line,
 } from "./pages";
 
 function App() {
@@ -62,11 +63,36 @@ function App() {
                     </div>
                     <div className="">
                         <Routes>
-                            {/* Dashboard */}
+                            {/* dashboard  */}
                             <Route path="/" element={<Ecommerce />} />
                             <Route path="/ecommerce" element={<Ecommerce />} />
 
-                            {/* */}
+                            {/* pages  */}
+                            <Route path="/orders" element={<Orders />} />
+                            <Route path="/employees" element={<Employees />} />
+                            <Route path="/customers" element={<Customers />} />
+
+                            {/* apps  */}
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route
+                                path="/color-picker"
+                                element={<ColorPicker />}
+                            />
+
+                            {/* charts  */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route
+                                path="/color-mapping"
+                                element={<ColorMapping />}
+                            />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
                         </Routes>
                     </div>
                 </div>
