3c2acc69171e5969f2099dec309315e3a425e7fe
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-03-28 02:14:37 -0400

    added invoices list

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -4,17 +4,64 @@ import { dummyOrders } from "../api/orders";
 import { OrderForm } from "../components";
 import { Link } from "react-router-dom";
 
+//invoice cards for list of invoices 
+const InvoiceCard = ({invoice}) => {
+    return (
+        
+        <div
+            className="flex flex-row justify-between gap-4 items-center border-2 m-2"
+        >
+            <div>
+                <span className=" font-bold capitalize">Invoice No.: </span>
+                {invoice["invoiceNumber"]}
+            </div>
+            <div>
+                <span className="">{invoice["invoiceDate"]}</span>
+            </div>
+            <div>
+                <span className="">{invoice["invoiceTotal"]}</span>
+            </div>
+        </div>
+    );
+};
+
 const Orders = () => {
+    const dummyInvoices = [
+        {
+            invoiceNumber: "A246",
+            invoiceDate: "01/08/2022",
+            invoiceTotal: 150,
+        },
+        {
+            invoiceNumber: "A247",
+            invoiceDate: "01/08/2022",
+            invoiceTotal: 150,
+        },
+        {
+            invoiceNumber: "A248",
+            invoiceDate: "01/08/2022",
+            invoiceTotal: 150,
+        },
+    ];
+
+
+
+    const invoices = dummyInvoices.map((invoice) => (
+        <InvoiceCard invoice={invoice} key={invoice['invoiceNumber']}/>
+    ));
+
     return (
         <div className="flex-col flex gap-4 justify-center items-center text-center">
             <p>Orders</p>
             <p>[Promotions, discounts, recommendations, etc. will go here]</p>
-            
-            <Link to='/addOrder'>
-                <button type="button" className="btn-primary">Add Order</button>
+
+            <Link to="/addOrder">
+                <button type="button" className="btn-primary">
+                    Add Order
+                </button>
             </Link>
 
-            <p>[todo: list of past orders/invoices here]</p>
+            <div>{invoices}</div>
         </div>
     );
 };
